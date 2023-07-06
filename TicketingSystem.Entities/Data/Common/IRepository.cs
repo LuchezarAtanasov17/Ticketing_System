7e6492076ff8443a3a5d91317be12cd33baee7df
Luchezar_a <lachezaratanasov1705@gmail.com>
2023-07-07 01:12:34 +0300

    Repository added.

--- TicketingSystem.Entities/Data/Common/IRepository.cs
+++ TicketingSystem.Entities/Data/Common/IRepository.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TicketingSystem.Entities.Data.Common
 {
-    internal class IRepository
+    public interface IRepository : IDisposable
     {
+        IQueryable<T> All<T>() where T : class;
+
+        Task<T> GetByIdAsync<T>(object id) where T : class;
+
+        Task AddAsync<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        Task DeleteAsync<T>(object id) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        Task<int> SaveChangesAsync();
     }
 }
--- TicketingSystem.Entities/Data/Common/Repository.cs
+++ TicketingSystem.Entities/Data/Common/Repository.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace TicketingSystem.Entities.Data.Common
 {
-    internal class Repository
+    public class Repository : IRepository
     {
+        protected DbContext Context { get; set; }
+
+        protected DbSet<T> DbSet<T>() where T : class
+        {
+            return this.Context.Set<T>();
+        }
+
+        public Repository(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>().AsQueryable();
+        }
+
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T entity = await GetByIdAsync<T>(id);
+
+            Delete<T>(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            EntityEntry entry = this.Context.Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet<T>().Attach(entity);
+            }
+
+            entry.State = EntityState.Deleted;
+        }
+
+        public void Dispose()
+        {
+            this.Context.Dispose();
+        }
+
+        public async Task<T> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await this.Context.SaveChangesAsync();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            this.DbSet<T>().Update(entity);
+        }
     }
 }
